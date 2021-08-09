71eb0d7293f2fc00bd9479f9d4b3f33c8de0c230
jomdab <shupak999@gmail.com>
2021-08-09 23:00:00 +0700

    s

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <Todo></Todo>
 
       {/* footer section */}
-      <p className='text-center text-gray-400'> ... </p>
+      <p className='text-center text-gray-400'> Copyright © 2021 </p>
     </div>
   );
 }
--- src/Task.tsx
+++ src/Task.tsx
@@ -1,32 +1,27 @@
-import { useState } from "react"
+import React,{useState} from 'react'
+type Props = {
+    id: Number;
+    name: String;
+    doneF: Function;
+    deleteF: Function; //Function type
+    finish: Boolean
+}
 
-type taskdata = {
-    id: Number
-    name: string
-    deleteF: Function
-    doneF: Function
-    finish: boolean
-  }
-
-const Task = ({id,name,deleteF,doneF,finish} :taskdata) => {
+const Task = ({ id, name, deleteF , doneF , finish }: Props) => {
     const [display,setdis] = useState<string>("none")
-    const onm1 = () => {
-        setdis("block")
-    }
-    const onm2 = () => {
-        setdis("none")
-    }
-    return(
-        <div
-            className="flex justify-between h-8 items-center py-6 border-b"
-            onMouseOver ={onm1} onMouseOut ={onm2}
-          >
-            <span className="text-2xl"> {name} </span>
-            <div className="flex space-x-1 items-center">
-            {finish? <button className="bg-green-400 w-24 text-2xl" onMouseOver ={onm1} style={{display: display}} onClick={doneF(id)}>Done</button> :null}
-            {finish? <button className="bg-red-400 w-24 text-2xl"  onMouseOver ={onm1}  style={{display: display}} onClick={deleteF(id)}>Delete</button> :null}
+    return (
+        <div>
+            <div
+            className="flex justify-between h-8 items-center py-6 border-b" onMouseOver={()=>setdis('block')} onMouseOut={()=>setdis('none')}
+            >
+                {finish?<span className="text-2xl"> {name} </span>:<span className="text-2xl"> <del>{name}</del> </span>}
+                <div className="flex space-x-1 items-center">
+                    {finish? <button className="bg-green-400 w-24 text-2xl" id="don" onClick ={()=>{doneF(id)}} onMouseOver={()=>setdis('block')} onMouseOut={()=>setdis('none')} style={{display:display}}>Done</button>:null}
+                    {finish? <button className="bg-red-400 w-24 text-2xl" id="del" onClick={()=>{deleteF(id)}} style={{display: display}} onMouseOut={()=>setdis('none')} onMouseOver={()=>setdis('block')}>Delete</button>:null}
+                </div>
             </div>
-          </div>
+        </div>
     )
 }
+
 export default Task
